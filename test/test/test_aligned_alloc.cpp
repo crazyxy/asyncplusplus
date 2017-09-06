ea5ff6e3398b229aca9c75f6e3c305b4c343713e
crazyxy <xueyansjtu@sjtu.edu.cn>
2017-09-06 13:47:43 +0800

    add c++11 support

--- test/test/test_aligned_alloc.cpp
+++ test/test/test_aligned_alloc.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 TEST(aligned_array, isAligned) {
 	using arr = typename async::detail::aligned_array<int>;
-	arr arr1(2);
+    size_t size = 2;
+	arr arr1(size);
 	cout << alignment_of<int>::value << endl;
 	double x = 1.0;
-	ASSERT_NEAR(x, 2.0, 1e-10);
+	ASSERT_NEAR(x, 1.0, 1e-10);
 }
